190c023b2e9ada9d07c144721445f322e4f9bb4b
Valentyn <valentyn28101993@ukr.net>
2020-07-26 22:40:54 +0300

    2hw-1attempt

---
+++ contacts.js
@@ -0,0 +1,106 @@
+const fs = require("fs");
+const path = require("path");
+const { promises: fsPromises } = fs;
+const { v4: uuidv4 } = require("uuid");
+
+const contactsPath = path.join(__dirname, "./db/contacts.json");
+
+async function listContacts() {
+  try {
+    const contacts = await fsPromises.readFile(contactsPath);
+
+    const parseContacts = JSON.parse(contacts);
+
+    console.table(parseContacts);
+
+    return parseContacts;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getContactById(contactId) {
+  try {
+    const contacts = await fsPromises.readFile(contactsPath);
+
+    const parseContacts = JSON.parse(contacts);
+
+    const findedContact = await parseContacts.find(
+      (contact) => contact.id === contactId
+    );
+
+    console.table(findedContact);
+
+    return findedContact;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const contacts = await fsPromises.readFile(contactsPath);
+
+    const parseContacts = JSON.parse(contacts);
+
+    const filteredContacts = parseContacts.filter(
+      (contact) => contact.id !== contactId
+    );
+    const deletedContact = parseContacts.find(
+      (contact) => contact.id === contactId
+    );
+    console.log("filteredContacts", filteredContacts);
+    await fsPromises.writeFile(contactsPath, JSON.stringify(filteredContacts));
+
+    return deletedContact;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function addContact(name, email, phone) {
+  try {
+    const oldContactsArr = await fsPromises.readFile(contactsPath);
+
+    const parseContacts = JSON.parse(oldContactsArr);
+
+    const newContact = { id: uuidv4(), name, email, phone };
+
+    const newContactsArr = [...parseContacts, newContact];
+
+    await fsPromises.writeFile(contactsPath, JSON.stringify(newContactsArr));
+
+    listContacts();
+    return newContact;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function updateContact(contactId, reqData) {
+  const oldContactsArr = await fsPromises.readFile(contactsPath);
+  const parseContacts = JSON.parse(oldContactsArr);
+  const findedContactIndex = await parseContacts.findIndex(
+    (contact) => contact.id === contactId
+  );
+  console.log("findedContactIndex", findedContactIndex);
+  parseContacts[findedContactIndex] = {
+    ...parseContacts[findedContactIndex],
+    ...reqData,
+  };
+  // console.log("updatedContact", updatedContact);
+  const updatedContactsArr = [...parseContacts];
+  console.log("updatedContactsArr", updatedContactsArr);
+
+  await fsPromises.writeFile(contactsPath, JSON.stringify(updatedContactsArr));
+  // console.log(updatedContact);
+  return parseContacts[findedContactIndex];
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+};
